118185c485a8e962f62cd4132d51cd603c0fc791
Luka Stekar <lukavladi@hotmail.com>
2021-05-30 21:52:07 -0300

    Agregando procfile

---
+++ index.js
@@ -0,0 +1,19 @@
+// importa la libreria express
+const express = require('express');
+
+// ponemos el puerto del server en una variable
+const port = process.env.PORT || 3000;
+
+// crea el objeto app
+const app = express();
+
+app.use(express.static('public'));
+
+// la app responde con Hello world
+// a todas las peticiones GET a /
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+// el server escucha en el puerto 3000
+app.listen(port);
